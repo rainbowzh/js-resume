952bf55496d169b325c6819d9c1b6285fbe82384
zhouhong07 <zhouhong07@58.com>
2020-07-21 19:19:13 +0800

    add new

--- src/App.js
+++ src/App.js
@@ -4,194 +4,33 @@
  * @Author: zhouhong07
  * @Date: 2019-10-10 16:39:42
  * @LastEditors: zhouhong07
- * @LastEditTime: 2020-07-17 13:58:33
+ * @LastEditTime: 2020-07-21 11:20:27
  */
 import React , { useEffect } from 'react';
 import './App.scss';
-import resolve from 'resolve';
+
 
 
 
 function App() {
   useEffect(() => {
-  //quickSort
-  const quickSort = (arr) => {
-    if(arr.length < 2) {
-      return arr ;
-    }
-    let temp = Math.floor(arr /2) ;
-    let flag = arrs.splice(temp,1)[0] ;
-    let right = [];
-    let left = [] ;
-    for(let i =0; i< arr.length ;i ++) {
-      if(arr[i] < temp) {
-        left.push(arr[i]);
-      }
-      else{
-        right.push(arr[i]);
-      }
-    }
-    return quickSort(left).concat(flag, quickSort(right));
-  }
-
-  //实现call
-  Array.prototype.mycall = function (context) {
-    if(typeof this !== 'funciton') {
-      throw new TypeError('not function');
-    }
-    context = context || window ;
-    context.fn = this ;
-    
-    let args = [...arguments].slice(1) ;
-    let result = contxt.fn(this,args);
-    delete context.fn ;
-    return result;
-  }
-
-  //promise
-  function myPromise (constructor) {
-    let self = this;
-    self.status = 'pending' ;
-    self.value = undefined ;
-    self.reason = undefined ;
-
-    function resolve (value) {
-      if(self.status == 'pending') {
-        self.value = valeu ;
-        self.status = 'resolved' ;
-      }
-    }
-
-    function rejecte (reason) {
-      if(self.status = 'pending') {
-        self.reason = reason ;
-        self.status = 'rejected' ;
-      }
-    }
-
-    try{
-      constructor(resolve, rejecte);
-    }catch(e) {
-      console.log(e);
-    }
-  }
-  myPromise.prototype.then = function(onfullfilled, onrejected) {
-    let self = this ;
-    switch (self.status) {
-      case 'resolved':
-        onfullfilled(self.value);
-        break;
-      case 'rejected' :
-        onrejected(self.reason) ;
-        break;
-      default:
-        break;
-    }
-  }
-
-  //jsonp
-  const myjsonp = (url, callBack='jsonpCallBack') => {
-    let Url = url.indexOf("?") !== -1 ? `${url}?callback=${callBack}` : `${url}&callback=${callBack}` ;
-    let domScirpt = document.createElement('script') ;
-    domScirpt.src = Url ;
-    document.body.appendChild(domScirpt) ;
-
-
-    return new Promise ((reslove, reject) => {
-      window[callBack] = (data) => {
-        try {
-          reslove(data) ;
-        } catch (error) {
-          reject(error) ;
-        } finally {
-          domScirpt.parentNode.removeChild(domScirpt);
+  //最长公共前缀
+  const maxStr = (strs)  => {
+    if (strs.length===0 ||strs[0].length===0){return "";}
+    var str=strs[0];
+    for (let i=1,len=strs.length;i<len;i++){
+        while(strs[i].indexOf(str)!==0) {
+            if (str.length === 0) {return "";}
+            str = str.substring(0, str.length - 1);
         }
-      }
-    });
+    }return str;
   }
-
-  //parseUrl
-  const parseUrl = (url) => {
-    const index = url.indexOf("?") ;
-    if(index == -1) {
-      return {} ;
-    }
-    let result = {}  ;
-    const arrTemp = url.slice(index+1).split("&") ;
-
-    for(let i in arrTemp) {
-      let temp = arrTemp[i].split('=') ;
-      let key = temp[0] ;let value = temp[1] ;
-      result[key] = value;
-    }
-    return result;
+  // maxStr(["aa","a"]);
+  maxStr(["flower","flow","flight"]);
+  
+  const s = () => {
+    let nums = [-4, -1, -1, 0, 1, 2];
   }
-
-  //apply
-  Array.prototype.myapply = function(context) {
-    if(typeof this !== 'funciton') {
-      throw new TypeError('not funciton') ;
-    }
-    context = context || window ;
-    context.fn = this; 
-    let result  ;
-    if(arguments[1]) {
-      result = context.fn(this, arguments[1]) ;
-    }else{
-      result = context.fn() ;
-    }
-    delete context.fn ;
-    return result ;
-  }
-  
-
-  //bind
-  Array.prototype.mybind = function (context) {
-    if(typeof this !== 'funciton') {
-      throw new TypeError('not funciton') ;
-    }
-    let args = arguments.slice(1) ;
-    let self = this ;
-    return function F() {
-      if(this instanceof F) {
-        return new self(context,arguments.concat(args)) ;
-      }else{
-        return self.apply(context, arguments.concat(args))
-      }
-    }
-  }
-
-  //throttle
-  const debonce = (fn, delay) => {
-    let timer = null ;
-    let self = this ;
-    return function() {
-      if(timer) {
-        timer = null ;
-      }
-      timer = setTimeout(() => {
-        self.apply(fn) ;
-        clearTimeout(timer);
-      },delay);
-    }
-  }
-
-
-  
-  //throttle
-  const throttle = (fn, delay) => {
-    let timer = new Date();
-    let self = this ;
-    return function() {
-      if(new Date() > timer + delay ){
-        self.apply(fn) ;
-      }
-    }
-  }
-  
-  
-  
-  
   },[]) ;
 
 
--- src/str.js
+++ src/str.js
@@ -4,7 +4,7 @@
  * @Author: zhouhong07
  * @Date: 2020-07-17 16:23:10
  * @LastEditors: zhouhong07
- * @LastEditTime: 2020-07-20 10:40:54
+ * @LastEditTime: 2020-07-21 11:27:26
  */ 
 
 var lengthOfLongestSubstring = function(s) {
@@ -67,3 +67,49 @@ var perm = function(s) {
   }
   return result;
 };  
+
+
+//三数之和
+var threeSum = function(nums) {
+    if(nums.length < 3){
+        return [];
+    }
+
+    let res = [];
+    // 排序
+    nums.sort((a, b) => a - b);
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] == nums[i-1]){
+            // 去重
+            continue;
+        }
+        if(nums[i] > 0){
+            // 若当前元素大于0，则三元素相加之后必定大于0
+            break;
+        }
+        // l为左下标，r为右下标
+        let l = i + 1; r = nums.length - 1;
+        while(l<r){
+            let sum = nums[i] + nums[l] + nums[r];
+            if(sum == 0){
+                res.push([nums[i], nums[l], nums[r]]);
+                while(l < r && nums[l] == nums[l+1]){
+                    l++
+                }
+                while(l < r && nums[r] == nums[r-1]){
+                    r--;
+                }
+                l++;
+                r--;
+            }
+            else if(sum < 0){
+                l++;
+            }
+            else if(sum > 0){
+                r--;
+            }
+        }
+    }
+
+    return res;
+};
